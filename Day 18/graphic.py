939d03d33efca597a92a2a71a84a660491fce988
Abhisek Das <166686275+Abhisek3005@users.noreply.github.com>
2024-06-16 16:16:34 +0530

    Add files via upload

---
+++ Day 18/graphic.py
@@ -0,0 +1,21 @@
+from turtle import *
+show = Turtle()
+show.shape("turtle")
+show.color("green")
+# show.forward(100)
+# show.left(90)
+# show.forward(100)
+# show.left(90)
+# show.forward(100)
+# show.left(90)
+# show.forward(100)
+for _ in range(15):
+#  show.penup()
+#  show.pendown()
+ show.forward(100)
+ 
+ show.left(90)
+
+
+screen = Screen()
+screen.exitonclick()
