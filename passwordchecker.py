1bd6ff97aedf704cf255e2543a8d96b56394ea19
Abhisek Das <166686275+Abhisek3005@users.noreply.github.com>
2024-06-16 16:19:04 +0530

    Delete passwordchecker.py

--- passwordchecker.py
+++
@@ -1,4 +0,0 @@
-username=input("enter your username")
-pawd=input("enter our password")
-lenghth='*' * len(pawd)#prints the star to 4 as  i mentioned  4 letter password.
-print(f'hey {username} your passwod is {lenghth} {len(pawd)} letters  long')
